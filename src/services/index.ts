e84c3447c0c86edbc35cb1dd48ea7cd2e19cb1cd
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-22 14:53:58 -0400

    feat: create episodes services for get all and types

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,7 @@
 import { Characters } from './characters';
+import { Episodes } from './episodes';
 
 export default class Services {
   static characters = Characters;
+  static episodes = Episodes;
 }
