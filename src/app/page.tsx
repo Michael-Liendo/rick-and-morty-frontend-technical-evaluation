c4691b6ab637338cd796d7e8618ecf6fb1d6f06c
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-22 14:42:52 -0400

    chore: list the data of characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
+'use client';
+
+import Services from '@/services';
+import { useEffect, useState } from 'react';
+import type { ICharacter } from '@/types/character';
+import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
+  const [characters, setCharacters] = useState<ICharacter[]>();
+
+  async function getAllCharacters() {
+    const results = await Services.characters.getAll();
+    setCharacters(results?.results);
+  }
+
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="">
+      <div className="flex justify-center items-center">
+        <div className="grid w-full max-w-sm items-center gap-1 mt-4">
+          <label htmlFor="find">Character</label>
+          <div className="flex items-center">
+            <Input
+              type="search"
+              id="find"
+              placeholder="Morty"
+              className="rounded-r-none"
+            />
+            <Button className="rounded-l-none bg-blue-600 hover:bg-blue-600/90">
+              Find
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 grid grid-cols-4 gap-8">
+        {characters?.map((character) => (
+          <Card key={character.id}>
+            <Image
+              src={character.image}
+              alt={`${character.name} photo`}
+              className="rounded-t-lg"
+              width={400}
+              height={500}
+            />
+            <CardContent>
+              <h1 className="text-2xl">{character.name}</h1>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
